e7858b0e49935c47c322868afdf639e8d73cc30d
Erlita20 <erlitadesy2000@gmail.com>
2020-09-17 06:05:17 +0700

    pratikum2

---
+++ PageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function hello(){
+        return 'Hello Word';
+    }
+
+   // public function home(){
+   //     return 'Selamat Datang';
+    //}
+
+    //public function about(){
+     //   return '1931710068-Erlita Desy Cahyuni';
+    //}
+
+    //public function articles($id){
+       // return 'Halaman artikel id '.$id;
+    //}
+}
